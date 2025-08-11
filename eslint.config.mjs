f08d682142562ba1dba17b6b9775ab9cca7f0d90
KianBahasadri <101868619+KianBahasadri@users.noreply.github.com>
2025-08-11 03:15:59 -0400

    oops

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignores: ['app/generated/**'],
+    ignorePatterns: ['app/generated/**'],
     rules: {
       '@typescript-eslint/no-unused-expressions': ['error', { allowShortCircuit: true, allowTernary: true, allowTaggedTemplates: true }],
     },
